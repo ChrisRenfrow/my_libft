13997e0c5d8348cb5b42abf1022c96c37f3f9712
ChrisRenfrow <mail@chrisrenfrow.me>
2016-10-22 12:35:10 -0700

    Final commit for submission I hope.

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: crenfrow <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/22 18:05:01 by crenfrow          #+#    #+#             */
-/*   Updated: 2016/10/20 13:38:41 by crenfrow         ###   ########.fr       */
+/*   Updated: 2016/10/22 12:34:35 by crenfrow         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,15 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n)
 	s1_cmp = (unsigned char *)s1;
 	s2_cmp = (unsigned char *)s2;
 	i = 0;
+	if (n == 0)
+		return (0);
 	while ((s1_cmp[i] && s2_cmp[i]) && i < n)
 	{
 		if (s1_cmp[i] - s2_cmp[i])
 			return (s1_cmp[i] - s2_cmp[i]);
 		i++;
 	}
-	if (i == n)
+	if (i == n && i > 0)
 		return (s1_cmp[i - 1] - s2_cmp[i - 1]);
 	else
 		return (s1_cmp[i] - s2_cmp[i]);
